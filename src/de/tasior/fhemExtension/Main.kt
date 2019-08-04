0c1c34dd9fc0e8a94f0ef11463a023c49798b8ba
Michael Tasior <michael@tasior.de>
2019-08-04 11:33:32 +0200

    Initialize External device for usage

--- src/de/tasior/fhemExtension/Main.kt
+++ src/de/tasior/fhemExtension/Main.kt
@@ -1,10 +1,11 @@
 package de.tasior.fhemExtension
 
+import de.tasior.fhemExtension.externalDevices.ExternalDevice
 import de.tasior.fhemExtension.fhem.FHEM
 
 fun main(args: Array<String>) {
     FHEM.setConnectionData(FHEM_HOST, FHEM_PORT)
     FHEM.startConnecting()
 
-
+    ExternalDevice() // This initializes the external device which connects itself to FHEM
 }
